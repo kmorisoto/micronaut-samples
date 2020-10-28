469d3f7d700a7fbcb6f8b75fb2eb220a0aa4b099
morisoto <k.morisoto@quo-digital.jp>
2020-10-28 09:42:52 +0900

    Add other error

---
+++ src/main/kotlin/com/example/KtlintError.kt
@@ -0,0 +1,34 @@
+package com.example
+
+class KtlintError {
+
+    // No semicolons.
+    fun error() {
+        val v = "";
+        println(v);
+    }
+}
+
+// No consecutive blank lines.
+// this in not a Python and you are not in PEP 8 realm
+class A {}
+
+
+class B {}
+
+// No trailing whitespaces.
+class C {}
+class D {}
+
+// No Unit returns.
+fun f(): Unit {}
+
+// No empty class bodies.
+class E {}
+
+interface F {}
+
+// Consistent string templates.
+val a = "class = ${String::class.toString()}"
+val b = "not ${a}"
+
